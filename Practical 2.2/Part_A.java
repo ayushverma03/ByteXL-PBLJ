2bffe6feb57a18df156f4e6de601671db61d1dc3
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-09-29 14:55:22 +0530

    Created Part_A.java

---
+++ Practical 2.2/Part_A.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SumUsingAutoboxing {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        System.out.println("Enter integers (type 'done' to finish):");
+        while (true) {
+            String input = sc.next();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
+            try {
+                int num = Integer.parseInt(input);
+                numbers.add(num); 
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, please enter an integer.");
+            }
+        }
+
+        int sum = 0;
+    
+        for (Integer num : numbers) {
+            sum += num; 
+        }
+
+        System.out.println("Sum of entered integers: " + sum);
+        sc.close();
+    }
+}
