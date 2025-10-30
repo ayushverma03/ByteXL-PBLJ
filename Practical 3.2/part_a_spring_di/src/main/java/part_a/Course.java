e22e5cffc3146b9465a4809c50340953c667034f
Ayush Verma <157206215+ayushverma03@users.noreply.github.com>
2025-10-30 14:32:42 +0530

    Create Course.java

---
+++ Practical 3.2/part_a_spring_di/src/main/java/part_a/Course.java
@@ -0,0 +1,17 @@
+package parta;
+
+public class Course {
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void displayCourse() {
+        System.out.println("Enrolled in course: " + courseName);
+    }
+}
